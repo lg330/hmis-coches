4bfd0cf31d365460456341f926db6752cbb41978
Luca Daniel Gavriloaie <lg330@inlumine.ual.es>
2023-06-26 13:18:45 +0200

    MainTest con cobertura al 100%

--- test/org/hmis/MainTest.java
+++ test/org/hmis/MainTest.java
@@ -1,5 +1,21 @@
 package org.hmis;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
 
+	@Test
+	void mainNoNulo() {
+		Main main = new Main();
+		assertNotNull(main);
+	}
+	
+	@Test
+	void mainFunciona() {
+		Main.main(null);
+	}
 }
